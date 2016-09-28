0c5ebca86b66b92385a627ce599312c6dcfef235
Benjamin <bsterner6696@gmail.com>
2016-09-28 13:31:46 -0500

    commented out unit tests

--- CustomListProject/CustomListUnitTests/UnitTest1.cs
+++ CustomListProject/CustomListUnitTests/UnitTest1.cs
@@ -7,21 +7,21 @@ namespace CustomListUnitTests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void GetLengthOfEmptyArrayLength5()
-        {
-            CustomList<string> basicList = new CustomList<string>();
-            int length = basicList.Length;
-            Assert.AreEqual(5, length);
+        //[TestMethod]
+        //public void GetLengthOfEmptyArrayLength5()
+        //{
+        //    CustomList<string> basicList = new CustomList<string>();
+        //    int length = basicList.Length;
+        //    Assert.AreEqual(5, length);
 
-        }
+        //}
 
-        [TestMethod]
-        public void GetLengthOfEmptyArrayLength0()
-        {
-            CustomList<string> basicList = new CustomList<string> { "cow", "pig", " ", " ", " ", " " };
-            int length = basicList.Length;
-            Assert.AreEqual(12, length);
-        }
+        //[TestMethod]
+        //public void GetLengthOfEmptyArrayLength12()
+        //{
+        //    CustomList<string> basicList = new CustomList<string> { "cow", "pig", " ", " ", " ", " " };
+        //    int length = basicList.Length;
+        //    Assert.AreEqual(12, length);
+        //}
     }
 }
